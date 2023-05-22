3cb1e8f2380705dc0383576658cb464fe61b3ab1
Mattedlopez <130605448+Mattedlopez@users.noreply.github.com>
2023-05-22 15:52:35 -0500

    Add files via upload
    commit 1

---
+++ matriz_tranpuesta.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int matriz[50][50];
+    int i, j;
+    int rows, columns;
+
+    printf("Ingresa en numero de filas: ");
+    scanf("%d", &rows);
+
+
+    return 0;
+}
