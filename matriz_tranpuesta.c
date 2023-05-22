e0c3dc3875ba4d5706402ee132607067135acf87
Mattedlopez <130605448+Mattedlopez@users.noreply.github.com>
2023-05-22 15:56:39 -0500

    commit 2

--- matriz_tranpuesta.c
+++ matriz_tranpuesta.c
@@ -7,6 +7,24 @@ int main(){
 
     printf("Ingresa en numero de filas: ");
     scanf("%d", &rows);
+    printf("Ingresa en numero de columnas: ");
+    scanf("%d", &columns);
+
+    for ( i = 0; i < rows; i++){
+        for ( j = 0; j < columns ; j++){
+            matriz[i][j]= rand()%100;
+        }
+    }
+
+    for ( i = 0; i < rows; i++){
+        for ( j = 0; j < columns ; j++){
+            printf("%d", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    
 
 
     return 0;
